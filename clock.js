f43564732d4db175f1c00aed8efd6fcf1b7afca7
Shaki1103 <164662270+Shaki1103@users.noreply.github.com>
2025-02-07 22:02:37 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -20,6 +20,11 @@ function displayTime(){
     hour.style.transform = `rotate(${hRotation}deg)`;
     min.style.transform = `rotate(${mRotation}deg)`;
     sec.style.transform = `rotate(${sRotation}deg)`;
+    
+
+    hour.style.-webkit-transform = `rotate(${hRotation}deg)`;
+    min.style.-webkit-transform = `rotate(${mRotation}deg)`;
+    sec.style.-webkit-transform = `rotate(${sRotation}deg)`;
 }
 
 setInterval(displayTime, 1000);
